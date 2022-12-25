22cac262d9e39c9be5773d8694b1735e81fae2fe
serge.muradian@gmail.com <serge.muradian@gmail.com>
2022-12-25 22:26:25 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+//flsFunctions.menu();
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
+
